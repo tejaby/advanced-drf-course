b950843782b6cf2f4a2314d754360c5a4a8ea20e
tejaby <yostintejaxun20@gmail.com>
2023-10-16 23:40:55 -0600

    add PUT, DELETE function based views for User

--- apps/user/api/api.py
+++ apps/user/api/api.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_204_NO_CONTENT
 
 from django.contrib.auth.models import User
 
@@ -9,7 +10,7 @@ from .serializers import UserSerializer
 @api_view(['GET', 'POST'])
 def user_api_view(request):
     if request.method == 'GET':
-        user = User.objects.all()
+        user = User.objects.filter(is_active = True)
         user_serializer = UserSerializer(user, many=True)
         return Response(user_serializer.data)
 
@@ -19,3 +20,33 @@ def user_api_view(request):
             user_serializer.save()
             return Response(user_serializer.data)
         return Response(user_serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, user_id):
+
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return Response(status=HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+    
+    elif request.method == 'PUT':
+        user_serializer = UserSerializer(user, data = request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        return Response(user_serializer.errors)
+
+    # elif request.method == 'DELETE':
+    #     user.is_active = False
+    #     user.save()
+    #     return Response(status=HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+    
+
--- apps/user/api/urls.py
+++ apps/user/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .api import user_api_view
+from .api import user_api_view, user_detail_api_view
 
 urlpatterns = [
     path('user/', user_api_view, name='list_user'),
+    path('user/<int:user_id>/', user_detail_api_view, name='detail_user'),
 ]
