59d6895a92aa724fcd3a0b2b6ce7dafff722a2e8
tejaby <yostintejaxun20@gmail.com>
2023-10-22 19:12:37 -0600

    add reusable view GeneralListAPIView

--- apps/product/api/views/general_views.py
+++ apps/product/api/views/general_views.py
@@ -1,12 +1,17 @@
 from rest_framework import generics
 
+from apps.base.api import GeneralListAPIView
+
 from apps.product.api.serializers.general_serializers import CategorySerializer
 
 from apps.product.models import Category
 
 
-class CategoryListAPIView(generics.ListAPIView):
-    serializer_class = CategorySerializer
+# class CategoryListAPIView(generics.ListAPIView):
+#     serializer_class = CategorySerializer
 
-    def get_queryset(self):
-        return Category.objects.filter(state=True)
+#     def get_queryset(self):
+#         return Category.objects.filter(state=True)
+    
+class CategoryListAPIView(GeneralListAPIView):
+    serializer_class = CategorySerializer
