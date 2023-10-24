56a7609925f688f2b7f2fa68ea12d572788822f9
tejaby <yostintejaxun20@gmail.com>
2023-10-23 18:43:07 -0600

    add class-based UpdateAPIViewviews for Product

--- apps/product/api/urls.py
+++ apps/product/api/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 
 from apps.product.api.views.general_views import CategoryListAPIView
-from apps.product.api.views.product_views import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductDestroyAPIView
+from apps.product.api.views.product_views import ProductListAPIView, ProductCreateAPIView, ProductRetrieveAPIView, ProductDestroyAPIView, ProductUpdateAPIView
 
 
 urlpatterns = [
     path('category/', CategoryListAPIView.as_view(), name='category'),
     path('product/list/', ProductListAPIView.as_view(), name='product_list'),
     path('product/create/', ProductCreateAPIView.as_view(), name='product_create'),
-    path('product/retrieve/<int:pk>',
+    path('product/retrieve/<int:pk>/',
          ProductRetrieveAPIView.as_view(), name='product_retrieve'),
-    path('product/destroy/<int:pk>',
-         ProductDestroyAPIView.as_view(), name='destroy'),
-
-
+    path('product/destroy/<int:pk>/',
+         ProductDestroyAPIView.as_view(), name='product_destroy'),
+    path('product/update/<int:pk>/',
+         ProductUpdateAPIView.as_view(), name='product_update'),
 ]
--- apps/product/api/views/product_views.py
+++ apps/product/api/views/product_views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework.generics import CreateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
@@ -13,7 +13,7 @@ class ProductListAPIView(GeneralListAPIView):
     serializer_class = ProductSerializer
 
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductCreateAPIView(CreateAPIView):
     serializer_class = ProductSerializer
 
     def post(self, request, *args, **kwargs):
@@ -24,7 +24,7 @@ class ProductCreateAPIView(generics.CreateAPIView):
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-class ProductRetrieveAPIView(generics.RetrieveAPIView):
+class ProductRetrieveAPIView(RetrieveAPIView):
     # queryset = Product.objects.filter(state=True) # usando atributo queryset
     serializer_class = ProductSerializer
 
@@ -32,18 +32,43 @@ class ProductRetrieveAPIView(generics.RetrieveAPIView):
         return self.get_serializer().Meta.model.objects.filter(state=True)
 
 
-class ProductDestroyAPIView(generics.DestroyAPIView):
+class ProductDestroyAPIView(DestroyAPIView):
     # queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_queryset(self):
         return self.get_serializer().Meta.model.objects.filter(state=True)
-    
+
     def delete(self, request, pk=None):
-        product = self.get_queryset().filter(id=pk).first()
-        print(product)
+        # product = self.get_queryset().filter(id=pk).first()
+        # usando el metodo get_object obtenemos el objeto basado en el valor pk
+        product = self.get_object()
         if product:
             product.state = False
             product.save()
             return Response({'message': 'Product deleted successfully'}, status=HTTP_200_OK)
         return Response({'message': 'Product not found'})
+
+
+class ProductUpdateAPIView(UpdateAPIView):
+    serializer_class = ProductSerializer
+    # queryset = Product.objects.all()
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.filter(state=True)
+
+    def patch(self, request, pk=None):
+        instance = self.get_queryset().filter(id=pk).first()
+        if instance:
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status=HTTP_200_OK)
+        return Response({'message': 'Product not found'})
+
+    def put(self, request, pk=None):
+        instance = self.get_queryset().filter(id=pk).first()
+        if instance:
+            serializer = self.get_serializer(instance, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=HTTP_200_OK)
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
