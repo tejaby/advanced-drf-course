9dcd2db864d4a38442e25b318841f97f3f9356de
tejaby <yostintejaxun20@gmail.com>
2023-10-26 04:28:18 -0600

    add class based view with APIView for logout

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,6 @@
 # ObtainAuthToken
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
 
 from rest_framework.response import Response
 
@@ -9,6 +10,8 @@ from rest_framework.authtoken.models import Token
 from apps.user.api.serializers import UserLoginSerializer
 from apps.user.api.serializers import UserTokenSerializer
 
+from rest_framework.authentication import TokenAuthentication
+
 """
 Vista basada en clase ObtainAuthToken para la autenticacion de usuarios y creacion de tokens
 - se puede utlizar tanto el serializer que ObtainAuthToken tiene definido o usar UserLoginSerializer
@@ -17,7 +20,7 @@ Vista basada en clase ObtainAuthToken para la autenticacion de usuarios y creaci
 """
 
 
-class CustomAuthToken(ObtainAuthToken):
+class CustomLoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(
             data=request.data, context={'request': request})
@@ -36,3 +39,26 @@ class CustomAuthToken(ObtainAuthToken):
         else:
             return Response({'error': 'username or password is invalid'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'token': token.key, 'user': user_serializer.data}, status=status.HTTP_200_OK)
+
+
+"""
+Vista basada en clase APIView para la autenticacion de usuarios y eliminacion de tokens
+- al agregar authentication_classes se podra enviar token en los headers y validarlos con:
+-request.user para representar al usuario autenticado. Además, establecerá 
+-request.auth para contener información relacionada con la autenticación, como el token  
+
+"""
+
+
+class CustomLogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request, *args, **kwargs):
+        token = request.auth
+        user = request.user
+        print(user)
+        if token:
+            token.delete()
+            return Response({'message': 'Token deleted successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'no token found'}, status=status.HTTP_400_BAD_REQUEST)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from apps.user.views import CustomAuthToken
+from apps.user.views import CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', CustomAuthToken.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('user/', include('apps.user.api.urls')),
     path('product/', include('apps.product.api.urls'))
 ]
