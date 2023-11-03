a5a7bdbbc69a033ece0913685e62d32f8347e661
Yostin Tejaxun <yostintejaxun20@gmail.com>
2023-11-03 03:32:06 -0600

    add django-cors-headers configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -42,6 +42,7 @@ DJANGO_APPS = [
 THIRD_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
+    "corsheaders",
 ]
 
 LOCAL_APPS = [
@@ -55,6 +56,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -92,6 +94,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
 
 
 # Database
